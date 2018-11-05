b6f0a7a607964cde5c92c7b3a2c13877c6db212c
Sergey Gerasimov <s.gerasimov@fbs.com>
2018-11-05 11:27:25 +0300

    added nsenter package

--- ns-exporter.go
+++ ns-exporter.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
+
+	"ns-exporter/nsenter"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
@@ -16,28 +18,38 @@ var (
 )
 
 func main() {
-	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.WithVersion(defaultDockerAPIVersion))
 	if err != nil {
 		panic(err)
 	}
 
-	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
 	if err != nil {
 		panic(err)
 	}
-	//fmt.Printf("%+v", containers)
 
 	for _, container := range containers {
 		contId = append(contId, container.ID)
 	}
 
 	for _, id := range contId {
-		stats, err := cli.ContainerInspect(ctx, id)
+		stats, err := cli.ContainerInspect(context.Background(), id)
 		if err != nil {
 			panic(err)
 		}
 		pids = append(pids, stats.State.Pid)
 	}
-	fmt.Println(pids)
+
+	for _, pid := range pids{
+		nscfg := nsenter.Config{
+			Mount:	true,
+			Target: pid,
+		}
+
+		stdout, _, err := nscfg.Execute("ss -nat")
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println(stdout)
+	}
 }
